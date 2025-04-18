b103192100ee1250864fb694a31731af49435dd6
Juliana Lima <165281819+limajulianaa@users.noreply.github.com>
2025-04-18 12:02:48 -0300

    Add files via upload

---
+++ exemplo2_for.py
@@ -0,0 +1,12 @@
+for item in range( 10 ) :
+    print( item )
+
+print("-------------------------------------")
+
+for item in range ( 5, 10 ) :
+    print( item )
+
+print("-------------------------------------")
+
+for item in range ( 1, 100, 10 ) :
+    print( item )
---
+++ exemplo4_for.py
@@ -0,0 +1,4 @@
+aluno = { "nome" : "João" , "idade" : 20 , "cpf" : "123.456.789.10"}
+
+for chave, valor in aluno.items() :
+    print( f"{chave} --> {valor}" )
