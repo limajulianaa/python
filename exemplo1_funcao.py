0cb49018b1c58e7fb2a55d409a3a3f6aaa08d186
Juliana Lima <165281819+limajulianaa@users.noreply.github.com>
2025-04-18 12:03:57 -0300

    Add files via upload

---
+++ exemplo1_funcao.py
@@ -0,0 +1,9 @@
+def exibir_mensagem() :
+    print("Bom dia!")
+
+def exibir_mensagem_nome( nome ) :
+    print(f"Bom dia {nome}. Seja bem vindo(a)")
+
+exibir_mensagem()
+exibir_mensagem_nome("Emily")
+exibir_mensagem_nome("Fábio")
---
+++ while2.py
@@ -0,0 +1,9 @@
+contador = 1 
+soma = 0
+
+while contador <= 100 :
+    soma = soma + contador
+    contador += 1
+
+print(f"A soma {soma}" )
+
